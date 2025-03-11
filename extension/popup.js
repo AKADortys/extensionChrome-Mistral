a05444a2100f2ad5d1af9e1871ca53acf3e5b80d
AKADortys <thibault.a@outlook.com>
2025-03-11 18:53:10 +0100

    Changement mineurs

--- extension/popup.js
+++ extension/popup.js
@@ -15,7 +15,11 @@ document.getElementById("resume-btn").addEventListener("click", () => {
           return;
         }
         Loader(container);
-        requestApi("resume", results[0].result, "Résume moi cette page: ")
+        requestApi(
+          "resume",
+          results[0].result,
+          "Résume moi cette page sans phrase d'introduction: "
+        )
           .then((data) => (container.innerHTML = data.response))
           .catch(() => (container.textContent = "Erreur API"));
       });
@@ -38,7 +42,32 @@ document.getElementById("translate-btn").addEventListener("click", () => {
         requestApi(
           "resume",
           results[0].result,
-          "Traduis moi le contenue de cette page: "
+          "Traduis moi le contenue de cette page en évitant hearder et footer: "
+        )
+          .then((data) => (container.innerHTML = data.response))
+          .catch(() => (container.textContent = "Erreur API"));
+      });
+  });
+});
+document.getElementById("search-btn").addEventListener("click", () => {
+  const prompt = document.getElementById("searchInput").value.trim();
+  const container = document.getElementById("container");
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.scripting
+      .executeScript({
+        target: { tabId: tabs[0].id },
+        function: findBody,
+      })
+      .then((results) => {
+        if (!results || !results[0]?.result) {
+          container.textContent = "Aucun résumé dispo";
+          return;
+        }
+        Loader(container);
+        requestApi(
+          "resume",
+          results[0].result,
+          prompt + " sois le plus court possible"
         )
           .then((data) => (container.innerHTML = data.response))
           .catch(() => (container.textContent = "Erreur API"));
